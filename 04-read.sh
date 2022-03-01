5af08cfddbf69e3183ba1dbb995f10ff9d73a17b
shuja <shuja.devops@gmail.com>
2022-03-01 20:03:52 +0530

    input var before exec

--- 04-read.sh
+++ 04-read.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-read -p "Enter user name" name
-useradd ${name}
-echo -e "\e[1;32m${name} user created successfully\e[0m"
+#read -p "Enter user name" name
+#useradd ${name}
+#echo -e "\e[1;32m${name} user created successfully\e[0m"
+
+echo "script name-" ${0}
+echo "number of arguments being passed " ${#}
