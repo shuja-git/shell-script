c0d8b9b364fe230d1f13e1bd0b13989ab3f0036b
shuja <shuja.devops@gmail.com>
2022-03-01 16:04:28 +0530

    fix m

--- 02-print.sh
+++ 02-print.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This is printing on screen using echo command
+# we can print in colors
 echo Installing Nginx
 echo -e "\e[1;32mStarting Nginx\e[0m"
-echo -e "\e[1;31Installing Nginx\e[0m"
+echo -e "\e[1;31mInstalling Nginx\e[0m"
