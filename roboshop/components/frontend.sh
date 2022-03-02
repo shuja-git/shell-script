d0159db0848e125a26434f8e339c1a43216fcbac
shuja <shuja.devops@gmail.com>
2022-03-02 17:56:06 +0530

    frontend script update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,7 +25,17 @@
 #--------------------------------
 echo Frontend setup
 #To Install Nginx.
-#yum install nginx -y
+yum install nginx -y
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx
+systemctl start nginx
 
 
 
