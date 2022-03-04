ad76fe782e061b39f692d552c6369b0cabcc412e
shuja <shuja.devops@gmail.com>
2022-03-04 18:50:33 +0530

    headings

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -27,7 +27,7 @@ source components/common.sh
 #Symbol < will take the input from a file and give that input to the command.
 #--------------------------------------------------------------------------
 
-echo -e " ---------->>>>>>>>>>>>\e[1;35mMongoDB Setup\e0m ------------<<<<<<<<  "
+echo -e " ---------->>>>>>>>>>>>\e[1;35mMongoDB Setup\e[0m ------------<<<<<<<<  "
 
 # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 
@@ -63,7 +63,7 @@ STAT_CHECK $? "Load Schema"
 
 #------------------------------------------
 #               REDIS
-echo -e " ---------->>>>>>>>>>>>\e[1;35mRedis Setup\e0m ------------<<<<<<<<  "
+echo -e " ---------->>>>>>>>>>>>\e[1;35mRedis Setup\e[0m ------------<<<<<<<<  "
 #Install Redis.
 
  curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
