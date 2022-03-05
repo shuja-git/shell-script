64b774d58acc2c4cb881e6423ffa3b095436307f
shuja <shuja.devops@gmail.com>
2022-03-05 12:03:25 +0530

    update RabbitMQ Script

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -103,9 +103,12 @@ rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
 STAT_CHECK $? "Create App user in RabbitMQ"
 fi
 
-rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE} && rabbitmqctl
-set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG_FILE}
-STAT_CHECK $? "Configure App user permissions"
+rabbitmqctl set_user_tags roboshop administrator  &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG_FILE}
+STAT_CHECK $? "Configure APp User Permissions"
+
+#rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE} && rabbitmqctl
+#set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG_FILE}
+#STAT_CHECK $? "Configure App user permissions"
 
 
 
