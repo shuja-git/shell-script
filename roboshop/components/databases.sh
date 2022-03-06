c5b54df2b49cf03be7fd9153b40a0dad721ca333
shuja <shuja.devops@gmail.com>
2022-03-06 11:44:28 +0530

    mysql change password

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -27,84 +27,84 @@ source components/common.sh
 #Symbol < will take the input from a file and give that input to the command.
 #--------------------------------------------------------------------------
 
-echo -e " ---------->>>>>>>>>>>>\e[1;35mMongoDB Setup\e[0m ------------<<<<<<<<  "
-
-# curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
-STAT_CHECK $? "Download MongoDB repo"
-
-
-
-# yum install -y mongodb-org
-yum install -y mongodb-org &>>${LOG_FILE}
-STAT_CHECK $? "MongoDB Installation"
-
-#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
-#Config file: /etc/mongod.conf
-
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG_FILE}
-STAT_CHECK $? "Update MongoDB Service"
-
-
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-DOWNLOAD mongodb
-
-
-# systemctl enable mongod
-# systemctl start mongod
-systemctl enable mongod &>>${LOG_FILE} && systemctl restart mongod &>>${LOG_FILE}
-STAT_CHECK $? "Start MongoDB Service"
-
-cd /tmp/mongodb-main
-
-mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
-STAT_CHECK $? "Load Schema"
-
-#------------------------------------------
-#               REDIS
-echo -e " ---------->>>>>>>>>>>>\e[1;35mRedis Setup\e[0m ------------<<<<<<<<  "
-#Install Redis.
-
- curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
-STAT_CHECK $? "Download Redis Repo"
-# yum install redis -y
-yum install redis -y &>>${LOG_FILE}
-STAT_CHECK $? "Install Redis"
-
-#Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${LOG_FILE}
-STAT_CHECK $? "Update Redis config"
-
-#Start Redis Database
-
-systemctl enable redis &>>${LOG_FILE} && systemctl restart redis &>>${LOG_FILE}
-STAT_CHECK $? "Update Redis"
-
-echo -e " ---------->>>>>>>>>>>>\e[1;35mRabbitMQ Setup\e[0m<<<<<<<<<<<<------------"
-
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
-STAT_CHECK $? "Download RabbitMQ repo"
-
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y rabbitmq-server -y &>>${LOG_FILE}
-STAT_CHECK $? "Install Erlang and RabbitMQ "
-
-
-systemctl enable rabbitmq-server &>>${LOG_FILE} &&  systemctl restart rabbitmq-server
-STAT_CHECK $? "RabbitMQ Started"
-#RabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect. Hence we need to create one user for the application.
-
-#Create application user
-rabbitmqctl list_users  | grep roboshop &>>${LOG_FILE}
-if [ $? -ne 0 ]; then
-#  echo -e "\e[1;33mUser already exists"
-#else
-rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
-STAT_CHECK $? "Create App user in RabbitMQ"
-fi
-
-rabbitmqctl set_user_tags roboshop administrator  &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG_FILE}
-STAT_CHECK $? "Configure App User Permissions"
+#echo -e " ---------->>>>>>>>>>>>\e[1;35mMongoDB Setup\e[0m ------------<<<<<<<<  "
+#
+## curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+#
+#curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
+#STAT_CHECK $? "Download MongoDB repo"
+#
+#
+#
+## yum install -y mongodb-org
+#yum install -y mongodb-org &>>${LOG_FILE}
+#STAT_CHECK $? "MongoDB Installation"
+#
+##Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+##Config file: /etc/mongod.conf
+#
+#sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG_FILE}
+#STAT_CHECK $? "Update MongoDB Service"
+#
+#
+## curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+#DOWNLOAD mongodb
+#
+#
+## systemctl enable mongod
+## systemctl start mongod
+#systemctl enable mongod &>>${LOG_FILE} && systemctl restart mongod &>>${LOG_FILE}
+#STAT_CHECK $? "Start MongoDB Service"
+#
+#cd /tmp/mongodb-main
+#
+#mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
+#STAT_CHECK $? "Load Schema"
+#
+##------------------------------------------
+##               REDIS
+#echo -e " ---------->>>>>>>>>>>>\e[1;35mRedis Setup\e[0m ------------<<<<<<<<  "
+##Install Redis.
+#
+# curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+#STAT_CHECK $? "Download Redis Repo"
+## yum install redis -y
+#yum install redis -y &>>${LOG_FILE}
+#STAT_CHECK $? "Install Redis"
+#
+##Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+#sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${LOG_FILE}
+#STAT_CHECK $? "Update Redis config"
+#
+##Start Redis Database
+#
+#systemctl enable redis &>>${LOG_FILE} && systemctl restart redis &>>${LOG_FILE}
+#STAT_CHECK $? "Update Redis"
+#
+#echo -e " ---------->>>>>>>>>>>>\e[1;35mRabbitMQ Setup\e[0m<<<<<<<<<<<<------------"
+#
+#curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
+#STAT_CHECK $? "Download RabbitMQ repo"
+#
+#yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y rabbitmq-server -y &>>${LOG_FILE}
+#STAT_CHECK $? "Install Erlang and RabbitMQ "
+#
+#
+#systemctl enable rabbitmq-server &>>${LOG_FILE} &&  systemctl restart rabbitmq-server
+#STAT_CHECK $? "RabbitMQ Started"
+##RabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect. Hence we need to create one user for the application.
+#
+##Create application user
+#rabbitmqctl list_users  | grep roboshop &>>${LOG_FILE}
+#if [ $? -ne 0 ]; then
+##  echo -e "\e[1;33mUser already exists"
+##else
+#rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
+#STAT_CHECK $? "Create App user in RabbitMQ"
+#fi
+#
+#rabbitmqctl set_user_tags roboshop administrator  &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG_FILE}
+#STAT_CHECK $? "Configure App User Permissions"
 
 #----------------------------------
 echo -e " ---------->>>>>>>>>>>>\e[1;35mMySQL Setup\e[0m<<<<<<<<<<<<------------"
@@ -153,3 +153,4 @@ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'"; >/tmp/pass.log
 #mysql -uroot -p"${DEFAULT_PASSWORD}" </tmp/pass.log
 
 mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/pass.log &>>${LOG_FILE}
+STAT_CHECK $? "Setup new root password"
