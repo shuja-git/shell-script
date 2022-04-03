9b72dc2d58115b6bad2e707e42072ac0edf7216b
shuja <shuja.devops@gmail.com>
2022-04-03 18:26:00 +0530

    catalogue script setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,8 +4,8 @@
 source components/common.sh
 MAX_LENGTH=$(cat ${0} components/databases.sh | grep -v -w cat | grep STAT_CHECK | awk -F '"' '{print $2}' | awk '{print length}' | sort | tail -1 )
 
-if [ MAX_LENGTH -lt 24 ]; then
-    MAX_LENGTH = 24
+if [ ${MAX_LENGTH} -lt 24 ]; then
+    MAX_LENGTH=24
 fi
 
 
