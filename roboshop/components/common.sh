66abe1449c076eddeb2786e1f32b3d4858244d4a
shuja <shuja.devops@gmail.com>
2022-04-03 21:59:38 +0530

    user component script

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -30,6 +30,39 @@ STAT_CHECK(){
  fi
 
 }
+NODEJS(){
+  component=${1}
+   yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
+  STAT_CHECK $? "Install Nodejs"
+
+  id roboshop &>>${LOG_FILE}
+  if [ $? -ne 0 ]; then
+   useradd roboshop &>>${LOG_FILE}
+   STAT_CHECK $? "Add Application User"
+  fi
+
+  #
+  DOWNLOAD ${component}
+  rm -rf /home/roboshop/${component} && mkdir -p /home/roboshop/${component} && cp -r /tmp/${component}-main/* /home/roboshop/${component} &>>${LOG_FILE}
+  STAT_CHECK $? "Copy ${component} Contents"
+  cd /home/roboshop/${component} && npm install --unsafe-perm &>>${LOG_FILE}
+  STAT_CHECK $? "Install Nodejs Dependencies"
+
+  chown roboshop:roboshop -R /home/roboshop
+
+
+  #NOTE: We need to update the IP address of MONGODB Server in systemd.service file
+  #Now, lets set up the service with systemctl.
+
+  sed -i 's/MONGO_DNSNAME/mongo.roboshop.internal/' /home/roboshop/${component}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service &>>${LOG_FILE}
+  STAT_CHECK $? "Update SystemD Config file"
+
+  # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+  systemctl daemon-reload &>>${LOG_FILE} && systemctl start ${component} &>>${LOG_FILE} && systemctl enable ${component} &>>${LOG_FILE}
+  STAT_CHECK $? "Start ${component} Service"
+
+}
+
 LOG_FILE=/tmp/roboshop.log
 rm -f ${LOG_FILE}
 
--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 source components/common.sh
+MAX_LENGTH=$(cat ${0} components/databases.sh | grep -v -w cat | grep STAT_CHECK | awk -F '"' '{print $2}' | awk '{print length}' | sort | tail -1 )
 
-Nodejs user
+if [ ${MAX_LENGTH} -lt 24 ]; then
+    MAX_LENGTH=24
+fi
+
+NODEJS user
+
+
+# curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
