d24e16e6ce2b451652230aad96550424e60dbaf7
shuja <shuja.devops@gmail.com>
2022-04-03 12:29:38 +0530

    RabbitMQ script setup

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -2,49 +2,49 @@
 
 source components/common.sh
 
-echo -e " ---------->>>>>>>>>>>>\e[1;35mMongoDB Setup\e[0m ------------<<<<<<<<  "
-
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
-STAT_CHECK $? "Download MongoDB repo"
+#echo -e " ---------->>>>>>>>>>>>\e[1;35mMongoDB Setup\e[0m ------------<<<<<<<<  "
 #
-yum install -y mongodb-org &>>${LOG_FILE}
-STAT_CHECK $? "MongoDB Installation"
-
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG_FILE}
-STAT_CHECK $? "Update MongoDB Service"
-
-systemctl enable mongod &>>${LOG_FILE} && systemctl restart mongod &>>${LOG_FILE}
-STAT_CHECK $? "Start MongoDB Service"
-
-#curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-
-DOWNLOAD mongodb
-
-cd /tmp/mongodb-main
-mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
-STAT_CHECK $? "Load Schema"
-
-
-
-
-#---------------------------
-##               REDIS
-echo -e " ---------->>>>>>>>>>>>\e[1;35mRedis Setup\e[0m ------------<<<<<<<<  "
-
-
-curl -L -s https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-STAT_CHECK $? "Download Redis Repo"
-
-yum install redis -y &>>${LOG_FILE}
-STAT_CHECK $? "Redis Installation"
-#Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
-STAT_CHECK $? "Update Redis"
-
-#Start Redis Database
-
-systemctl enable redis &>>${LOG_FILE} &&  systemctl start redis &>>${LOG_FILE}
-STAT_CHECK $? "Redis Started"
+#curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
+#STAT_CHECK $? "Download MongoDB repo"
+##
+#yum install -y mongodb-org &>>${LOG_FILE}
+#STAT_CHECK $? "MongoDB Installation"
+#
+#sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG_FILE}
+#STAT_CHECK $? "Update MongoDB Service"
+#
+#systemctl enable mongod &>>${LOG_FILE} && systemctl restart mongod &>>${LOG_FILE}
+#STAT_CHECK $? "Start MongoDB Service"
+#
+##curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+#
+#DOWNLOAD mongodb
+#
+#cd /tmp/mongodb-main
+#mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
+#STAT_CHECK $? "Load Schema"
+#
+#
+#
+#
+##---------------------------
+###               REDIS
+#echo -e " ---------->>>>>>>>>>>>\e[1;35mRedis Setup\e[0m ------------<<<<<<<<  "
+#
+#
+#curl -L -s https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+#STAT_CHECK $? "Download Redis Repo"
+#
+#yum install redis -y &>>${LOG_FILE}
+#STAT_CHECK $? "Redis Installation"
+##Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+#sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+#STAT_CHECK $? "Update Redis"
+#
+##Start Redis Database
+#
+#systemctl enable redis &>>${LOG_FILE} &&  systemctl start redis &>>${LOG_FILE}
+#STAT_CHECK $? "Redis Started"
 
 
 
