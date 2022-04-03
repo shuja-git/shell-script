a688ae9e3b1090e05fd76bb3442c3133b6dc9474
shuja <shuja.devops@gmail.com>
2022-04-03 18:25:04 +0530

    catalogue script setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo Catalogue setup
+#echo Catalogue setup
 source components/common.sh
 MAX_LENGTH=$(cat ${0} components/databases.sh | grep -v -w cat | grep STAT_CHECK | awk -F '"' '{print $2}' | awk '{print length}' | sort | tail -1 )
-#echo MAX_LENGTH
+
+if [ MAX_LENGTH -lt 24 ]; then
+    MAX_LENGTH = 24
+fi
+
 
 
 
