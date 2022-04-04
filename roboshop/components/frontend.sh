603b61ffd324f7fbf6bdce337bb265fe5b643de5
shuja <shuja.devops@gmail.com>
2022-04-04 22:02:49 +0530

    for loop used

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -17,11 +17,14 @@ cd /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $? "Copy Nginx Config file"
 
-  sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' \
-          -e '/cart/ s/localhost/cart.roboshop.internal/' \
-          -e '/user/ s/localhost/user.roboshop.internal/' \
-          -e '/shipping/ s/localhost/shipping.roboshop.internal/' \
-          -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+for component in catalogue cart user shipping payment; do
+  sed -i -e "/${component}/ s/localhost/${component}.roboshop.internal" /etc/nginx/default.d/roboshop.conf
+done
+#  sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' \
+#          -e '/cart/ s/localhost/cart.roboshop.internal/' \
+#          -e '/user/ s/localhost/user.roboshop.internal/' \
+#          -e '/shipping/ s/localhost/shipping.roboshop.internal/' \
+#          -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
   STAT_CHECK $? "Update Nginx Config file"
 
 systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
