94753aaa7b54bc160f4c4b209dd3f04457aee8dc
shuja <shuja.devops@gmail.com>
2022-04-04 13:15:13 +0530

    dispatch service update

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -17,7 +17,8 @@ SYSTEMD_SETUP(){
            -e 's/DBHOST/mysql.roboshop.internal/' \
            -e 's/CARTHOST/cart.roboshop.internal/' \
            -e 's/USERHOST/user.roboshop.internal/' \
-           -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${component}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service &>>${LOG_FILE}
+           -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
+            -e 's/RABBITMQ-IP/rabbitmq.roboshop.internal/' /home/roboshop/${component}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service &>>${LOG_FILE}
     STAT_CHECK $? "Update SystemD Config file"
 
     # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
